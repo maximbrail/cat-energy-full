69c07ac35b18f99ad14e0a306ebe800244408e45
maximbrail <maxabobus953@gmail.com>
2024-08-30 16:57:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
